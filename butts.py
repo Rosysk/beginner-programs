49f70d5b17086c0b4312990fe25d9e516cc85b7c
rosysk <shiori@shiori.us>
2014-09-28 21:18:09 -0700

    added butts

---
+++ butts.py
@@ -0,0 +1,16 @@
+spaces = ' ' * 25
+print('%s 1278 dolphin drive' %spaces)
+print('%s Twintklestar cloud' %spaces)
+print('%s jumper USA' %spaces)
+print
+print
+print('Dear dolphin')
+print
+print('how are you doing?i am doing great.')
+print('I would love to come over to your house.')
+print('I am open all tuesdays.')		
+print('are you open?')
+print('see you!')
+print
+print('from')
+print('shiori')
