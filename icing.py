2de6907d1ecf3b6254e64655444e8a48bd7fa1c8
rosysk <shiori@shiori.us>
2014-09-29 07:58:50 -0700

    get a message for your age!

---
+++ icing.py
@@ -0,0 +1,15 @@
+
+if age < 50:
+    print('you are awsome and young!')
+    print('do you like cake?')
+    print('the icing is organic!')
+    if age<12:
+        print('kids eat free at my restraut!')
+        print('at saint patricks day you even might find a four leaf clover!')
+        print('please come soon!')
+        
+if age < 47:
+    print('you did a great job!')
+    print('do you want to have tea time?')
+    print('lets have one!')
+    
